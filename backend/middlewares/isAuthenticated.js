cde098e7034bddbf7a2a9ff600e9edf4790e1dbb
Mohammad Ali <aliuser1129@gmail.com>
2026-01-29 01:38:23 +0530

    Changed

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,23 +1,25 @@
 import jwt from "jsonwebtoken";
 
-const isAuthenticated = (req, res, next) => {
-  try {
-    const authHeader = req.header("Authorization");
-
-    if (!authHeader) {
-      return res.status(401).json({ message: "No token provided" });
+const isAuthenticated = async (req, res, next) => {
+    try {
+        const token = req.cookies.token;
+        if (!token) {
+            return res.status(401).json({
+                message: "User not authenticated",
+                success: false,
+            })
+        }
+        const decode = jwt.verify(token, process.env.SECRET_KEY);
+        if(!decode){
+            return res.status(401).json({
+                message:"Invalid token",
+                success:false
+            })
+        };
+        req.id = decode.userId;
+        next();
+    } catch (error) {
+        console.log(error);
     }
-
-    const token = authHeader.split(" ")[1]; // Bearer TOKEN
-
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    req.id = decoded.userId;
-
-    next();
-  } catch (error) {
-    console.log(error);
-    return res.status(401).json({ message: "Invalid token" });
-  }
-};
-
+}
 export default isAuthenticated;
