633bcdbe164871a52dc65395f54e606b18ec0e02
Mohammad Ali <aliuser1129@gmail.com>
2026-01-29 01:35:02 +0530

    Changed

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,14 +1,23 @@
+import jwt from "jsonwebtoken";
+
 const isAuthenticated = (req, res, next) => {
   try {
     const authHeader = req.header("Authorization");
-    if (!authHeader) return res.status(401).json({ message: "No token" });
 
-    const token = authHeader.split(" ")[1];
+    if (!authHeader) {
+      return res.status(401).json({ message: "No token provided" });
+    }
+
+    const token = authHeader.split(" ")[1]; // Bearer TOKEN
+
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
-
     req.id = decoded.userId;
+
     next();
-  } catch (err) {
+  } catch (error) {
+    console.log(error);
     return res.status(401).json({ message: "Invalid token" });
   }
 };
+
+export default isAuthenticated;
