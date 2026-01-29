f7153adfd86cdec79cf3547286b44c61f72980e4
Mohammad Ali <aliuser1129@gmail.com>
2026-01-30 00:35:13 +0530

    cors updated

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const corsOptions = {
     // // origin:'https://hirenest-theta.vercel.app',
     origin: [
     "http://localhost:5173",
-    "https://hirenest-theta.vercel.app"
+    "https://hirenest-job-portal-2.onrender.com/"
   ],
     credentials:true
 }
